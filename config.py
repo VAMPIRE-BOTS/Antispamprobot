0aa6275a1c05ec0ccaa73d423ee57838f2e85b0d
VAMPIRE-BOTS <mdhakeeb11@gmail.com>
2026-05-09 17:19:01 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+API_ID = 12345678
+
+API_HASH = "YOUR_API_HASH"
+
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+
+OWNER_ID = 8628886006
+
+BOT_USERNAME = "VampGuardXBot"
+
+MUST_JOIN = "VAMPIREUPDATES"
+
+LOG_GROUP_ID = -1001234567890
+
+PHOTO_URL = "https://i.ibb.co/Gf10dmKx/x.jpg"
+
+MONGO_URL = "mongodb+srv://username:password@cluster.mongodb.net/?retryWrites=true&w=majority"
