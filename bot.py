76e0af2ee631caefdf9600b721c264e422f8b833
VAMPIRE-BOTS <mdhakeeb11@gmail.com>
2026-05-09 18:30:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,14 @@
 from pyrogram import Client
 from config import *
 
-plugins = {"root": "app.modules"}
+plugins = dict(root="app.modules")
 
 app = Client(
-    name="VampGuardX",
+    "VampGuardX",
     api_id=int(API_ID),
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=plugins,
-    workers=50
+    plugins=plugins
 )
 
 print("🛡️ VampGuard X Started Successfully ✅")
