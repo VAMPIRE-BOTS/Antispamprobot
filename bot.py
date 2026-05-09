5585c02e0d86cbf42e0f141a3a2084433e3fcf98
VAMPIRE-BOTS <mdhakeeb11@gmail.com>
2026-05-09 17:19:43 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+from pyrogram import Client
+from config import *
+
+plugins = dict(root="app.modules")
+
+app = Client(
+    "VampGuardX",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=plugins
+)
+
+print("🛡️ VampGuard X Started Successfully")
+
+app.run()
